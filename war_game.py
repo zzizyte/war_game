2ecac895529a5b15a122ec2e621993125eb8686a
zzizyte <102749983+zzizyte@users.noreply.github.com>
2024-06-13 12:47:05 +0000

    first commit/comments

---
+++ war_game.py
@@ -0,0 +1,7 @@
+#create deck
+#randomize deck
+#separate to two decks
+#both players open first card. Higher value wins
+#winner puts the cards at the end of the deck
+# if value of cards are equal, both players open second card face down and another card face up.
+#repeat value check
